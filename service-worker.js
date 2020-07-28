0f1bd4e30256989bf861744928bb00e8b37d732d
moritz brandl <moritz.brandl@technikum-wien.at>
2020-07-28 21:16:53 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.660b69bfd846e691af91f6bc04d4b3d7.js"
+  "/precache-manifest.3c80cfd3258df0978cc5245766deff81.js"
 );
 
 self.addEventListener('message', (event) => {
