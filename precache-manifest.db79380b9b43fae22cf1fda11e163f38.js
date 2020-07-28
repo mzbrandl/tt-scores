04bb80f8caf78e033271b34155325ca28f460aa1
Moritz Brandl <moritz.brandl@atos.net>
2020-07-28 14:39:25 +0200

    Updates

---
+++ precache-manifest.db79380b9b43fae22cf1fda11e163f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1363ed7ab99bd2bf9c7c3635dfecbeb",
+    "url": "/tt-scores/index.html"
+  },
+  {
+    "revision": "ba1b791fa7938deacc98",
+    "url": "/tt-scores/static/css/main.03d386b4.chunk.css"
+  },
+  {
+    "revision": "6fc9d7b4a90a57ccb30a",
+    "url": "/tt-scores/static/js/2.390410ca.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/tt-scores/static/js/2.390410ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba1b791fa7938deacc98",
+    "url": "/tt-scores/static/js/main.b0caab1b.chunk.js"
+  },
+  {
+    "revision": "2b5c139edf504a369e98",
+    "url": "/tt-scores/static/js/runtime-main.9306de48.js"
+  }
+]);
